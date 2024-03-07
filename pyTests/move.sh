72563730f730b61ab61f8b4ffa2e72777614cd51
GeneralBlaze <benjaminprincewill@gmail.com>
2024-03-07 15:39:52 +0100

    bash to move files to new folder

---
+++ pyTests/move.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Create a directory to store merged files
+mkdir -p merged_files
+
+# Move all files with "merged" in their filenames to the merged_files directory
+mv *merged* merged_files/
+
