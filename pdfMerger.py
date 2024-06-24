3ac9284626623d727d215d96c44a3e9a6bba7079
GeneralBlaze <benjaminprincewill@gmail.com>
2024-06-24 16:48:12 +0100

    added sorting to pdf so the files merge orderly

--- pdfMerger.py
+++ pdfMerger.py
@@ -2,10 +2,13 @@ import os
 from PyPDF2 import PdfMerger
 
 # Prompt for the directory
-directory = r"/Users/princewill/alx-interview/CLIMAX BASH/inserted docs"
+directory = r"/Users/princewill/alx-interview/CLIMAX BASH/REFUND LETTERS"
+
+#sort list of pdf for easy locating
+sorted_list = sorted(os.listdir(directory))
 
 # Find all .pdf files in the directory
-pdf_files = [f for f in os.listdir(directory) if f.endswith("pdf")]
+pdf_files = [f for f in sorted_list if f.endswith("pdf")]
 
 print(f"Found {len(pdf_files)} PDF files.")
 
@@ -22,7 +25,7 @@ for filename in pdf_files:
         print(f"Failed to process {filename}: {e}")
 
 # Write the merged .pdf file to disk
-output_filename = os.path.join(directory, "merged.pdf")
+output_filename = os.path.join(directory, "order REFUND LETTERS.pdf")
 merger.write(output_filename)
 merger.close()
 
